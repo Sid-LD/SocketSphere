944436d436b798a58b0f18dd8207e1304172367f
Siddhant Roy <siddhantroy2006@gmail.com>
2026-06-24 16:35:50 +0530

    feat: add Dockerfile and serve static frontend assets

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,31 +1,39 @@
 import express from "express";
 import "dotenv/config";
-import {connectDB} from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 import User from "./models/user.model.js";
 import { clerkMiddleware } from "@clerk/express";
-import cors from 'cors'
-
-
-
+import cors from "cors";
+import fs from "fs";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const FRONTEND_URL=process.env.FRONTEND_URL
+const FRONTEND_URL = process.env.FRONTEND_URL;
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(clerkMiddleware());
-app.use(express.json())
-app.use(cors({origin:FRONTEND_URL, credentials:true}))
+app.use(express.json());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 
-app.get('/health', (req, res)=>{
-    res.status(200).json({
-        ok: true,
-        message: "Server is healthy"
-    });
-})
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "Server is healthy",
+  });
+});
+
+//// if the public directory exists send the static file
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 
-app.listen(PORT, ()=>{
-    connectDB();
-    console.log("Server is running on port " + PORT);
-    
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port " + PORT);
+});
