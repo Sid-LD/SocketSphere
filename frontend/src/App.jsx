bafc7c57fe0d96ae474233e9762a46611107a2ab
Siddhant Roy <siddhantroy2006@gmail.com>
2026-06-25 20:25:05 +0530

    feat: implement theme and wallpaper context providers
    - Added ThemeContext and WallpaperContext for managing theme and wallpaper state.
    - Created herouiThemePresets.js for theme presets and wallpapers.js for wallpaper data.
    - Introduced ThemeProvider and WallpaperProvider components to wrap the application.
    - Implemented light/dark mode and theme preset functionality.
    - Added AuthPage and ChatPage components for routing.
    - Updated main.jsx to include BrowserRouter for navigation.
    - Removed vite.svg asset as it is no longer needed.
    - Updated index.css to integrate Tailwind CSS and HeroUI styles.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,29 @@
-
-import './App.css'
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+import { Show, SignInButton, SignUpButton, useAuth, UserButton } from "@clerk/react";
+import { Button } from "@heroui/react";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { WallpaperProvider } from "./context/WallpaperContext.jsx";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage.jsx";
+import AuthPage from "./pages/AuthPage.jsx";
 
 function App() {
+  const {isSignedIn, isLoaded}=useAuth()
 
+  //make this a better component
+  if(!isLoaded) return <p> loading...</p>
 
   return (
-    <>
-      <div>
-        <h1>My app</h1>
-        <header>
-          <Show when="signed-out">
-            <SignInButton mode='model' />
-            <SignUpButton mode='model' />
-          </Show>
-          <Show when="signed-in">
-            <UserButton />
-          </Show>
-        </header>
-      </div>
-    </>
+    <ThemeProvider>
+      <WallpaperProvider>
+
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage/> : <Navigate to={"/auth"} replace /> } />
+          <Route path="/auth" element={!isSignedIn ? <AuthPage/> : <Navigate to={"/chat"} replace />} />
+        </Routes>
+
+      </WallpaperProvider>
+    </ThemeProvider>
   );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthPage = () => {
+  return (
+    <div>
+      Authpage
+    </div>
+  )
+}
+
+export default AuthPage
